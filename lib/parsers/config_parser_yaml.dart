aa4d0c33d53dc7a950352d4f0ffe58df06ccda3a
jheyne <jim.heyne@centurylink.com>
2018-06-11 10:11:44 -0500

    Handle YamlMap

--- lib/parsers/config_parser_yaml.dart
+++ lib/parsers/config_parser_yaml.dart
@@ -1,17 +1,21 @@
 library config_parser_yaml;
 
-import 'dart:async';
 import 'package:yaml/yaml.dart' as YAML;
 
 import '../config.dart';
 
 class YamlConfigParser implements ConfigParser {
-
   @override
   Map<String, dynamic> parse(String configText) {
     var map = YAML.loadYaml(configText);
-    print('YMAL result is ${map.runtimeType}');
-    if(map is Map<String, dynamic>) {
+    if (map is YAML.YamlMap) {
+      print('YMAL result is a YamlMap');
+      Map<String, dynamic> asMap = {};
+      for (String key in map.keys) {
+        asMap[key] = map[key];
+      }
+      return asMap;
+    } else if (map is Map<String, dynamic>) {
       print('YMAL result is a Map');
       return map;
     } else {
